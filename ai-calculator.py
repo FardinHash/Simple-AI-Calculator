3dfa6d8c12ec91c35107d25536c3da6802ad59c6
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-11-23 12:49:34 +0600

    Update ai-calculator.py

--- ai-calculator.py
+++ ai-calculator.py
@@ -1,21 +1,21 @@
 from chatterbot import ChatBot
 
-Bot = ChatBot(name = 'Calculator',
+Bot = ChatBot(name = 'Calculator',  #name
                 read_only = True,                  
-                logic_adapters = ['chatterbot.logic.MathematicalEvaluation'],                 
-                storage_adapter = 'chatterbot.storage.SQLStorageAdapter')
+                logic_adapters = ['chatterbot.logic.MathematicalEvaluation'],  #mathematical function                 
+                storage_adapter = 'chatterbot.storage.SQLStorageAdapter')  #SQLDB
     
 print('\033c')
 print('Yoo, calculator speaking, What to do?')
 while (True):
-    user_input = input('me: ')
+    user_input = input('me: ')  #taking input from the user
     
-    if user_input.lower() == 'quit':
+    if user_input.lower() == 'quit':  #to exit from calculator
         print('Bye')
         break
         
     try:
-        response = Bot.get_response(user_input)
-        print('Calculator: ', response)
+        response = Bot.get_response(user_input)  #processing the answare
+        print('Calculator: ', response)  #get the answare
     except:
-        print('Calculator: Please enter some valid input bro')
+        print('Calculator: Please enter some valid input bro')  #wrong, input again
